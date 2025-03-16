62d23ab1debc234a1b6c9670db8222e518be5a8c
Helivelton Barbosa <helivelton.gomes@gmail.com>
2025-03-16 18:33:13 -0300

    add var

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    if (process.env.PORT) {
+      return `Hello World! ${process.env.PORT}. ${process.env.OTHER_ENV_VAR}`;
+    }
     return 'Hello World!';
   }
 }
